7687f3704a7ef60596b7dc6ae334312730329e7f
Keith <keith@custombit.com>
2014-05-09 11:52:28 -0600

    Set up comments controller/views.
    Again, views are very rudimentary.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(safe_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def safe_params
+    params.require(:comment).permit(:comment, :user_id)
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
   belongs_to :user
+  has_many :comments
 
   scope :ordered, order('created_at ASC')
 end
