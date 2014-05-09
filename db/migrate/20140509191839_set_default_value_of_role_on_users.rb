4b679b352a3769fa9c49aff338dbce4f00ca81ec
Keith <keith@custombit.com>
2014-05-09 13:29:10 -0600

    fix typo in migration file

--- db/migrate/20140509191839_set_default_value_of_role_on_users.rb
+++ db/migrate/20140509191839_set_default_value_of_role_on_users.rb
@@ -1,5 +1,5 @@
 class SetDefaultValueOfRoleOnUsers < ActiveRecord::Migration
   def change
-    update_column :users, :role, :string, :default => 'commenter'
+    change_column :users, :role, :string, :default => 'commenter'
   end
 end
