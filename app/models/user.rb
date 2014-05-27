51b7029561fb9537f5496e60bfe685564808c098
Keith <keith@custombit.com>
2014-05-27 16:56:03 -0600

    Disable password requirement on user edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :create
 
   has_many :comments
   has_many :posts
