33524f8b52eeeea907f336e3ae2a97938bacdb12
Keith <keith@custombit.com>
2014-06-04 16:20:25 -0600

    Add basic admin functions for administrators

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -82,6 +82,6 @@ class UsersController < ApplicationController
     end
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user) || current_user.admin?
     end
 end
